a83054fe83742a11eda3342d4e0734ecf7d545ae
Helen Oresanya <103387732+HELEN-DEE@users.noreply.github.com>
2023-09-10 22:18:55 +0100

    Internal corrections

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const currentDate = new Date();
-// console.log(today)
+function updateContent() {
+  const currentDate = new Date();
 
-const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  const daysOfTheWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-const currentDayOfTheWeek = daysOfTheWeek[currentDate.getDay()]
-const currentUTCTime = Date.now()
+  const currentDayOfTheWeek = daysOfTheWeek[currentDate.getDay()];
+  const currentUTCTime = Date.now();
 
-document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent= `Current Day of the Week is ${currentDayOfTheWeek}`
+  document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  ).textContent = `Current Day of the Week is ${currentDayOfTheWeek}`;
 
-document.querySelector('[data-testid="currentUTCTime"]').textContent= `Current Time is ${currentUTCTime}`
+  document.querySelector(
+    '[data-testid="currentUTCTime"]'
+  ).textContent = `Current Time is ${currentUTCTime}`;
+}
 
-
-
+// ? This Set Interval function runs
+setInterval(() => {
+  updateContent();
+}, 1000);
